2fc1b637979a0fca648236512fa5a7294ef442ea
Rubenicos <44579213+Rubenicos@users.noreply.github.com>
2022-12-13 17:55:03 -0600

    Added cooldown to commands

--- dynamicpremium-bungee/src/main/java/me/gatogamer/dynamicpremium/bungee/commands/FullPremiumCommand.java
+++ dynamicpremium-bungee/src/main/java/me/gatogamer/dynamicpremium/bungee/commands/FullPremiumCommand.java
@@ -8,15 +8,19 @@ import me.gatogamer.dynamicpremium.commons.utils.UUIDUtils;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.config.Configuration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FullPremiumCommand extends Command {
 
+    private final Map<String, Long> cooldown = new HashMap<>();
     private final List<String> confirm = new ArrayList<>();
 
     public FullPremiumCommand(String name) {
@@ -25,20 +29,35 @@ public class FullPremiumCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        Configuration mainSettings = DynamicPremium.getInstance().getMainSettings();
         if (!sender.hasPermission("dynamicpremium.fullpremium")) {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Admin.No Permission")));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', DynamicPremium.getInstance().getMainSettings().getString("Admin.No Permission")));
             return;
         }
         if (sender instanceof ProxiedPlayer) {
             ProxiedPlayer player = (ProxiedPlayer) sender;
+
+            final long time = System.currentTimeMillis();
+            if (cooldown.containsKey(player.getName())) {
+                final long playerTime = cooldown.get(player.getName());
+                if (playerTime > time) {
+                    final long seconds = (playerTime - time) / 1000;
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',
+                            DynamicPremium.getInstance().getMainSettings().getString("CooldownMessage").replace("%seconds%", String.valueOf(seconds))
+                    )));
+                    return;
+                }
+            }
+
+            Configuration mainSettings = DynamicPremium.getInstance().getMainSettings();
+            cooldown.put(player.getName(), time + mainSettings.getLong("PremiumCommandDelay", 60000));
+
             Cache cache = DynamicPremium.getInstance().getCacheManager().getOrCreateCache(player.getName());
 
             ProxyServer.getInstance().getScheduler().runAsync(DynamicPremium.getInstance(), () -> {
                 Database database = DynamicPremium.getInstance().getDatabaseManager().getDatabase();
                 PlayerState state = database.playerState(player.getName());
                 if (state == PlayerState.FULL_PREMIUM) {
-                    player.disconnect(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.Disabled")));
+                    player.disconnect(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.Disabled"))));
                     database.removePlayer(player.getName());
                 } else if (!UUIDUtils.isPremium(player.getName())) {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.NoPremium")));
@@ -47,13 +66,13 @@ public class FullPremiumCommand extends Command {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.FullPremium")));
                 } else {
                     confirm.remove(player.getName());
-                    player.disconnect(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.Checking").replace("/premium", "/fullpremium")));
+                    player.disconnect(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.Checking").replace("/premium", "/fullpremium"))));
                     cache.setPendingVerification(true);
                     cache.setFullPremium(true);
                 }
             });
         } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.PlayerOnly")));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', DynamicPremium.getInstance().getMainSettings().getString("Alert.PlayerOnly")));
         }
     }
 }
--- dynamicpremium-bungee/src/main/java/me/gatogamer/dynamicpremium/bungee/commands/PremiumCommand.java
+++ dynamicpremium-bungee/src/main/java/me/gatogamer/dynamicpremium/bungee/commands/PremiumCommand.java
@@ -10,20 +10,42 @@ import me.gatogamer.dynamicpremium.commons.utils.UUIDUtils;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.config.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PremiumCommand extends Command {
+
+    private final Map<String, Long> cooldown = new HashMap<>();
+
     public PremiumCommand(String name) {
         super(name);
     }
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        Configuration mainSettings = DynamicPremium.getInstance().getMainSettings();
         if (sender instanceof ProxiedPlayer) {
             ProxiedPlayer player = (ProxiedPlayer) sender;
+
+            final long time = System.currentTimeMillis();
+            if (cooldown.containsKey(player.getName())) {
+                final long playerTime = cooldown.get(player.getName());
+                if (playerTime > time) {
+                    final long seconds = (playerTime - time) / 1000;
+                    player.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',
+                            DynamicPremium.getInstance().getMainSettings().getString("CooldownMessage").replace("%seconds%", String.valueOf(seconds))
+                    )));
+                    return;
+                }
+            }
+
+            Configuration mainSettings = DynamicPremium.getInstance().getMainSettings();
+            cooldown.put(player.getName(), time + mainSettings.getLong("PremiumCommandDelay", 60000));
+
             Cache cache = DynamicPremium.getInstance().getCacheManager().getOrCreateCache(player.getName());
 
             ProxyServer.getInstance().getScheduler().runAsync(DynamicPremium.getInstance(), () -> {
@@ -35,13 +57,13 @@ public class PremiumCommand extends Command {
                 } else if (!UUIDUtils.isPremium(player.getName())) {
                     player.sendMessage(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.NoPremium")));
                 } else {
-                    player.disconnect(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.Checking")));
+                    player.disconnect(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.Checking"))));
                     cache.setPendingVerification(true);
                     cache.setFullPremium(false);
                 }
             });
         } else {
-            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', mainSettings.getString("Alert.PlayerOnly")));
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', DynamicPremium.getInstance().getMainSettings().getString("Alert.PlayerOnly")));
         }
     }
 }
